9fd86380593e79ed42dcbf74e4db7cebeda67bb1
majk <michalmidor1@gmail.com>
2025-04-08 16:40:41 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>F1 Tracker</h1>
+      <p>Minimalistyczna aplikacja do zarządzania wyścigami</p>
+    </div>
+  );
+}
+
+export default App;
