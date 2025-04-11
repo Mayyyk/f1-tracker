df182516062f63a1e5a2d6c8ce3b3922d840ee35
Mayyyk <michalmidor1@gmail.com>
2025-04-11 12:44:28 +0200

    Temp rename raceApi.js to fix casing

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import raceRoutes from "./routes/races.js"
-
+import dotenv from 'dotenv-flow';
+dotenv.config();
 
 const app = express();
 
@@ -23,12 +24,14 @@ app.use(express.json());
 
 app.use("/api/races", raceRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  console.log("Running in production with Railway DB");
+} else {
+  console.log("Running in dev with local DB");
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
